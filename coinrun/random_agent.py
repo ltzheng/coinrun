e5b1fd3819e04194fce799203b188758499ab8d7
Longtao Zheng <longtao001@e.ntu.edu.sg>
2024-06-21 15:31:38 +0800

    Simplify code

--- coinrun/random_agent.py
+++ coinrun/random_agent.py
@@ -2,59 +2,55 @@ import numpy as np
 from coinrun import setup_utils, make
 import os
 from PIL import Image
-from tqdm import tqdm
 
 
-def random_agent(num_episodes=10000, max_steps=1000):
-    os.makedirs("dataset", exist_ok=True)
-    os.makedirs("dataset/images", exist_ok=True)
+def random_agent(file_stem, num_episodes=10, max_steps=1000):
+    # remove existing dataset
+    if os.path.exists(file_stem):
+        os.system(f"rm -r {file_stem}")
+    os.makedirs(os.path.join(file_stem, "dataset/images"))
     setup_utils.setup_and_load()
 
-    num_envs = 100
-    assert num_episodes % num_envs == 0
-    num_iters = num_episodes // num_envs
-
-    for i in tqdm(range(num_iters), desc="Iter"):
-        metadata = [[] for _ in range(num_envs)]
-        print(f"Episode {i * num_envs}-{(i + 1) * num_envs}")
+    for i in range(num_episodes):
+        metadata = []
+        print(f"Episode {i}")
         last_acts = None
-        env = make('standard', num_envs=num_envs)
+        env = make('standard', num_envs=1)
 
-        env.step(np.array([0] * num_envs))
+        env.step(np.array([0]))
         obs = env.get_images()
-        for j in range(num_envs):
-            image = Image.fromarray(obs[j])
-            image.save(f"dataset/images/episode_{i + j}_step_0.jpg")
-            metadata[j].append({"obs": f"dataset/images/episode_{i + j}_step_0.jpg"})
+        image = Image.fromarray(obs[0])
+        img_filename = f"episode_{i}_step_0.jpg"
+        image.save(os.path.join(file_stem, "dataset/images", img_filename))
+        metadata.append({"obs": img_filename})
 
-        for step in tqdm(range(max_steps), desc="Steps"):
+        for step in range(max_steps):
             acts = np.array([env.action_space.sample() for _ in range(env.num_envs)])
             # no action repeat
             if last_acts is not None:
-                for j in range(num_envs):
-                    while last_acts[j] == acts[j]:
-                        acts[j] = env.action_space.sample()
-            last_acts = acts
+                while last_acts == acts[0]:
+                    acts = np.array([env.action_space.sample() for _ in range(env.num_envs)])
+            last_acts = acts[0]
             _, rews, dones, _ = env.step(acts)  # obs shape (num_envs, 64, 64, 3)
             obs = env.get_images()
+            image = Image.fromarray(obs[0])
+            img_filename = f"episode_{i}_step_{step + 1}.jpg"
+            image.save(os.path.join(file_stem, "dataset/images", img_filename))
 
-            for j in range(num_envs):
-                image = Image.fromarray(obs[j])
-                image.save(f"dataset/images/episode_{i + j}_step_{step + 1}.jpg")
-                dict_to_add = {
-                    "acts": acts[j].tolist(),
-                    "obs": f"images/episode_{i + j}_step_{step + 1}.jpg",
-                    "rews": rews[j].tolist(),
-                    "dones": dones[j],
-                }
-                metadata[j].append(dict_to_add)
+            dict_to_add = {
+                "acts": acts[0].tolist(),
+                "obs": img_filename,
+                "rews": rews[0].tolist(),
+                "dones": dones[0],
+            }
+            metadata.append(dict_to_add)
 
         env.close()
 
-        with open(f"dataset/metadata.jsonl", "a") as f:
-            for m in metadata:
-                f.write(f"{m}\n")
+        with open(os.path.join(file_stem, f"dataset/metadata.jsonl"), "a") as f:
+            f.write(f"{metadata}\n")
 
 
 if __name__ == '__main__':
-    random_agent()
+    file_stem = "/media/longtao/DATA/Genie/"
+    random_agent(file_stem=file_stem)
